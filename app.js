fd492b6b7b534dad46786b70e397a0ce3d95dbcb
yuansy <sinyi5997@gmail.com>
2020-03-15 21:54:32 +0800

    add quiz

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const modal = document.getElementById("myModal");
-
 speciesDict = {
     1: {
         'name': 'Nyireh', 
@@ -7,6 +5,10 @@ speciesDict = {
         'content': {
             'Where seen?': 'The handsome Mangrove cannon-ball tree (Xylocarpus granatum) is still commonly seen in our mangroves. But other Xylocarpus species are not as frequently encountered.', 
             'Features': 'Generally trees, the different species have tiny flowers that appear rather similar. The species are best distinguished by where they are growing, and structures of the leaves, bark, roots and fruits.'
+        },
+        'quest': {
+            'question': 'Questionsssss???',
+            'answer': 'true'
         }
     },
     2: {
@@ -15,6 +17,22 @@ speciesDict = {
         'content': {
             'Where seen?': 'Mudskippers are commonly seen on many of our shores. They are particularly abundant in mangroves and muddy shores, but are also seen on rocky shores and near reefs.', 
             'Features': "Those seen about 6-12cm, some species can be much larger or smaller. Mudskippers are well adapted to the intertidal area. Being able to stay of water for a while gives mudskippers an advantage over 'normal' fishes. During low tide, they are among the few marine creatures that can exploit the dry muddy or sandy flats. More about how to tell apart small mudskippers commonly found on our shores."
+        },
+        'quest': {
+            'question': 'Questionsssss2222?',
+            'answer': 'false'
+        }
+    },
+    3: {
+        'name': '', 
+        'species': '', 
+        'content': {
+        }
+    },
+    4: {
+        'name': '', 
+        'species': '', 
+        'content': {
         }
     }
 }
@@ -31,31 +49,39 @@ function createContainer(i) {
     image.setAttribute('id', 'image'+i);
     image.setAttribute('src', 'assets/container_background.jpg');
     container.appendChild(image);
-    
-    let buttonList = document.querySelector('.btn-group');
-    let button = document.createElement('button')
-    button.setAttribute('class', 'btn btn-secondary');
-    button.setAttribute('id', 'button'+i)
-    button.setAttribute('onclick', 'solved(this)');
-    button.textContent = i;
-    buttonList.appendChild(button);
+}
 
+for (i=1; i<=12; i++) {
+    createContainer(i);
+}
+
+// Submit image
+var input = document.querySelector('#file-upload');
+input.addEventListener('change', checkImage);
+
+function checkImage(event) {
+    var input = event.srcElement;
+    var fileName = input.files[0].name.split('.')[0];
+    if (Object.keys(speciesDict).includes(fileName)) {
+        solved(fileName);
+    } else {
+        alert('unrecognised')
+    }
 }
 
 // Species is solved
-function solved(button) {
-    let i = button.getAttribute('id').replace('button', '');
-
+function solved(i) {
     let image = document.querySelector('#image'+i);
     image.setAttribute('src', 'assets/'+i+'.jpeg');
+    image.setAttribute('class', 'solved');
     image.setAttribute('onclick', 'showInfo(this)');
-
 }
 
 // Show Modal
+var modal = document.getElementById("myModal");
+
 function showInfo(image) {
     let i = image.getAttribute('id').replace('image', '');
-    console.log(i);
     modal.style.display = "block";
 
     let name = document.querySelector('#name');
@@ -73,6 +99,21 @@ function showInfo(image) {
         detail.textContent = pairs[1];
         content.appendChild(detail);
     }
+
+    let question = document.querySelector('#question');
+    question.textContent = speciesDict[i]['quest']['question'];
+
+    let trueRadio = document.querySelector('#true');
+    trueRadio.checked = false;
+    let falseRadio = document.querySelector('#false');
+    falseRadio.checked = false;
+    let answer = document.querySelector('#answer');
+    answer.innerHTML = "";
+
+    var btn = document.querySelector('.submit');
+    btn.setAttribute('id', 'button'+i);
+    btn.setAttribute('onclick', 'checkAnswer(this)');
+
 }
 
 // Close Modal <x>
@@ -81,7 +122,20 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
+// Check Answer
+function checkAnswer(btn) {
+    let i = btn.getAttribute('id').replace('button', '');
+    let answer = document.querySelector('#answer');
 
-for (i=1; i<=12; i++) {
-    createContainer(i);
+    let trueRadio = document.querySelector('#true');
+    let falseRadio = document.querySelector('#false');
+    if ((trueRadio.checked & speciesDict[i]['quest']['answer']=='true') | (falseRadio.checked & speciesDict[i]['quest']['answer']=='false')) {
+        answer.innerHTML = "CORRECT!";
+        answer.style.color = "green";
+    } else {
+        answer.innerHTML = "WRONG :("
+        answer.style.color = "red";
+    }
 }
+
+solved(1);
